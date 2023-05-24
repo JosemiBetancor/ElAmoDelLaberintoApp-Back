c1a1fa9bcd81165bbd2d498babc535c857981341
JosemiBetancor <josemiguelbetancorvelazquez@hotmail.com>
2023-05-24 17:58:19 +0100

    CORS added to media controller

--- src/main/java/com/minik/laberinto/controller/MediaController.java
+++ src/main/java/com/minik/laberinto/controller/MediaController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("media")
 @AllArgsConstructor
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class MediaController {
 	private final StorageService storageservice;
 	private final HttpServletRequest request;
